2d44a3844322e534c46a3d5023da7daef7804132
sizzsaint <80381476+sizzsaint@users.noreply.github.com>
2021-05-13 14:52:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,39 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="EUC-KR">
-<title>회원가입</title>
-
-<link href="style.css" rel="stylesheet" type="text/css">
-<!-- css포함 -->
-<script language="JavaScript" src="script.js"></script>
-<!-- js포함 -->
-
-</head>
-
-<body bgcolor="#996600">
-<table width="80%" align="center" border="0" cellspacing="0" cellpadding="5">
-<tr>
-<td align="center" valign="middle" bgcolor="#FFFFCC">
-
-<table width="90%" border="1" cellspacing="0" cellpadding="2" align="center">
-<form name="regForm" method="post" action="RegisterProc.jsp">
-
-<tr align="center" bgcolor="#996600">
-<td colspan="3"><font color="#FFFFFF"><b>회원가입</b></font></td>
-</tr>
-<tr>
-<td width="24%">아이디</td>
-<td width="35%"><input type="text" name="mem_id" size="15"></td>
-<td width="41%">아이디를 적어 주세요.</td>
-</tr>
-<tr>
-<td>패스워드</td>
-<td><input type="password" name="mem_passwd" size="15"></td>
-<td>패스워드를 적어주세요.</td>
-</tr>
-<tr>
-<td>패스워드확인</td>
-<td><input type="password" name="mem_repasswd" size="15"></td>
-<td>패스워드를 확인합니다.</td>
-</tr>
-<tr>
-<td>이름</td>
-<td><input type="text" name="mem_name" size="15"></td>
-<td>고객실명을 적어주세요</td>
-</tr>
-<tr>
-<td>이메일</td>
-<td><input type="text" name="mem_email" size="27"></td>
-<td>이메일을 적어주세요</td>
-</tr>
-<tr>
-<td>전화번호</td>
-<td><input type="text" name="mem_phone" size="20"></td>
-<td>연락처를 적어주세요</td>
-</tr>
-<tr>
-<td colspan="3" align="center">
-<input type="button" value="회원가입" onclick="inputCheck()">
-<!--  유효성검사 -->
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<input type ="reset" value="다시쓰기">
-</td>
-</tr>
-</form>
-</table>
-</td>
-</tr>
-</table>
-
-</body>
-</html>
+function inputCheck(){
+	if(document.regForm.mem_id.value==""){
+		alert("아이디를 입력해 주세요");
+		document.regForm.mem_id.focus();
+		return;
+	}
+	if(document.regForm.mem_passwd.value==""){
+		alert("비밀번호를 입력해 주세요");
+		document.regForm.mem_passwd.focus();
+		return;
+	}
+	if(document.regForm.mem_repasswd.value==""){
+		alert("비밀번호를 입력해 주세요");
+		document.regForm.mem_repasswd.focus();
+		return;
+	}
+	if(document.regForm.mem_name.value==""){
+		alert("이름을 입력해주세요");
+		document.regForm.mem_name.focus();
+		return;
+	}
+	if(document.regForm.mem_email.value==""){
+		alert("이메일을 입력해주세요");
+		document.regForm.mem_email.focus();
+		return;
+	}
+	if(document.regForm.mem_phone.value==""){
+		alert("연락처를 입력해주세요");
+		document.regForm.mem_phone.focus();
+		return;
+	}
+if(document.regForm.mem_passwd.value != document.regForm.mem_repasswd.value){
+	alert("비밀번호가 일치하지 않습니다.");
+	document.regForm.mem_repasswd.focus();
+	return;
+}
+document.regForm.submit();
+	
+}
